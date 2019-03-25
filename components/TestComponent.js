c18afdc0d718a30ff2ef7e9b1a10dcd2361cf49f
HAQish <mytrueemailaddress@gmail.com>
2019-03-25 14:56:23 -0700

    xml serializer npm installed

--- components/TestComponent.js
+++ components/TestComponent.js
@@ -15,7 +15,9 @@ class TestComponent extends React.Component {
     this.state = {
       default: "true",
       message: "notChangedYet",
-      url: null
+      url: null,
+      spinner: false,
+      formattedText: null
     }
     //bindings
     this.testPress = this.testPress.bind(this);
@@ -34,44 +36,82 @@ class TestComponent extends React.Component {
     this.setState({ url: text });
   }
 
-  getSubtitles() {
+  async getSubtitles() {
     console.log("getting subtitles");
+    //https://www.youtube.com/watch?v=FIORjGvT0kk&list=PL4cUxeGkcC9gfoKa5la9dsdCNpuey2s-V
+    //https://www.youtube.com/watch?v=XJGiS83eQLk&t=0h3m57s
+    // via regex (could also just slice)
+    const vidId = this.state.url.match(/\/watch\?v=(\w+)&?/i)[1];
+    const baseStr = "https://www.youtube.com/api/timedtext?lang=en&v=";
+    const link = baseStr + vidId;
+    this.setState({ spinner: true });
+    console.log("link being queried", link);
+    let xmlResponse = await axios.get(link);
+    console.log("xmlResponse", xmlResponse);
+    let serializer = new XMLSerializer();
+    var formattedText = serializer.serializeToString(xmlResponse)
+                                  .replace(/<text start="[\d\.]+" dur="[\d\.]+">[\s\n]+<\/text>/g, "")
+                                  .replace(/\n{2,}/, "")
+                                  .replace(/\n/g, "\r\n")
+                                  .replace(/<\/text>/g, "\r\n")
+                                  .replace(/<[^>]+>/g, "")
+                                  .replace(/&amp;#39;/g, "'")
+                                  .trim();
+    this.setState({ formattedText, spinner: false });
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.inputRow}>
-          <TextInput 
-            onChangeText={this.inputChange}
-            placeholder="Video URL"
-            placeholderTextColor="#333333"
-            style={
-              {
-                paddingLeft: 3, 
-                borderWidth: 1, 
-                borderRadius: 5, 
-                borderColor: "black",
-                width: "75%",
-                maxWidth: 300,
-                padding: 2,
-                paddingLeft: 4,
-                height: 40
-              }
-            }
-          >
-          </TextInput>
-          <TouchableOpacity
-            color="#ffedc7"
-            onPress={this.getSubtitles}
-            style={styles.getButton}
-            activeOpacity={0.5}
-          >
-            <Text style={{fontSize: 16, color: "black"}}> Get Subtitles </Text>
-          </TouchableOpacity>
+    if (this.state.spinner) {
+      return (
+        <View>
+          <Text>
+            Loading...
+          </Text>
         </View>
-      </View>
-    )
+      )
+    } else if (this.state.formattedText) {
+      return (
+        <View>
+          <Text>
+            {this.state.formattedText}
+          </Text>
+        </View>
+      )
+    } else {
+      return (
+        <View style={styles.container}>
+          <View style={styles.inputRow}>
+            <TextInput 
+              onChangeText={this.inputChange}
+              placeholder="Video URL"
+              placeholderTextColor="#333333"
+              style={
+                {
+                  paddingLeft: 3, 
+                  borderWidth: 1, 
+                  borderRadius: 5, 
+                  borderColor: "black",
+                  width: "75%",
+                  maxWidth: 300,
+                  padding: 2,
+                  paddingLeft: 4,
+                  height: 40
+                }
+              }
+            >
+            </TextInput>
+            <TouchableOpacity
+              color="#ffedc7"
+              onPress={this.getSubtitles}
+              style={styles.getButton}
+              activeOpacity={0.5}
+            >
+              <Text style={{fontSize: 16, color: "black"}}> Get Subtitles </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      )
+    }
   }
 }
 
